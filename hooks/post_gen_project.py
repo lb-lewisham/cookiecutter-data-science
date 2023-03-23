449227e1dc6bf9bdd3339a934c24dbfc143dcd66
Ilkka-LBL <ilkka.sipila@lewisham.gov.uk>
2023-03-23 15:53:45 +0000

    reformatted final destination

--- hooks/post_gen_project.py
+++ hooks/post_gen_project.py
@@ -4,4 +4,5 @@ import shutil
 source_dir = os.getcwd()
 target_dir = "{{ cookiecutter.final_destination }}"
 
-shutil.move(source_dir, target_dir)
+if len(target_dir) > 3:
+    shutil.move(source_dir, target_dir)
