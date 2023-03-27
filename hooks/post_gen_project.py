6d373913f5568b713ca31a5e80705db101097d8d
Ilkka-LBL <ilkka.sipila@lewisham.gov.uk>
2023-03-27 10:16:43 +0100

    add models path

--- hooks/post_gen_project.py
+++ hooks/post_gen_project.py
@@ -4,5 +4,9 @@ import shutil
 source_dir = os.getcwd()
 target_dir = "{{ cookiecutter.final_destination }}"
 
+models_dir = os.path.join(source_dir, 'models')
+if not os.path.exists(models_dir):
+    os.makedirs(models_dir)
+
 if len(target_dir) > 3:
     shutil.move(source_dir, target_dir)
